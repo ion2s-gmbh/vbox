abc90cc0b7124f9ef615301a8f0cc34d728f14e3
JD <58289234+jdtron@users.noreply.github.com>
2019-11-29 12:34:28 +0100

    Fix loading of nvm

--- provisioning/loadNvm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export NVM_DIR="/home/vagrant/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
--- provisioning/nvm.sh
+++ provisioning/nvm.sh
@@ -4,7 +4,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-source /vagrant/provisioning/loadNvm.sh
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 nvm install --lts=Dubnium --latest-npm
 nvm alias default stable
