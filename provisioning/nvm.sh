fe8b925bac559b2e557cb708866f6ca340ed2e9e
JD <58289234+jdtron@users.noreply.github.com>
2019-11-29 14:06:50 +0100

    Fix exporting of nvm installation directory

--- provisioning/nvm.sh
+++ provisioning/nvm.sh
@@ -4,8 +4,8 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-export NVM_DIR="/home/vagrant/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install --lts=Dubnium --latest-npm
 nvm alias default stable
