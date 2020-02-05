c57524f74078442ac4f05d69dbf9a5fb06807ce4
Sebastian Baum <sebastian.baum@ion2s.com>
2020-02-05 12:13:22 +0100

    Integrated SSL with Apache

--- provisioning/apache.sh
+++ provisioning/apache.sh
@@ -5,10 +5,11 @@ apt-get install apache2 -y
 adduser vagrant www-data
 
 # Copy the vhost config
-cp /vagrant/provisioning/templates/pache/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp /vagrant/provisioning/templates/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
 
 sudo a2enmod proxy_fcgi setenvif
 sudo a2enconf php7.2-fpm
-sudo systemctl reload apache2
+sudo a2enmod ssl
+sudo systemctl restart apache2
 
 exit $?
